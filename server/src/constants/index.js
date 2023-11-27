7a6e485c6bd633ae64639f1eec1dd1bc0e91b46c
deusnyakundi <nyakundideus1@gmail.com>
2023-11-27 16:26:54 +0300

    Implementing routes for authentication

--- server/src/constants/index.js
+++ server/src/constants/index.js
@@ -5,5 +5,5 @@ module.exports = {
     PORT: process.env.PORT,
     secret: process.env.SECRET,
     CLIENT_URL: process.env.CLIENT_URL,
-    SERVER_URL:process.env.SERVER_URL
+    SERVER_URL:process.env.SERVER_URL,
 }
--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,22 @@
-import React from 'react';
+/*import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+*/
 
 const express = require('express')
 const app = express()
 
+const {PORT} = require ('./constants')
+
+
+//import routes
+
+const authRoutes = require('./routes/auth')
+
+//initialize routes 
+app.use('/api', authRoutes)
 //APP start
 
 const appStart = () => {
@@ -22,7 +32,7 @@ const appStart = () => {
 
 appStart()
 
-
+/*
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -33,4 +43,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(); */
