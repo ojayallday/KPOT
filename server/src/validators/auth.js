73fefcbd10b650fcdd3a6c53ae90b63e3e6ea2f3
deusnyakundi <nyakundideus1@gmail.com>
2023-11-28 00:23:29 +0300

    testing errors with express validator middleware and postman

--- server/src/validators/auth.js
+++ server/src/validators/auth.js
@@ -3,15 +3,19 @@ const db = require ('../db')
 
 //password
 
-const password = check ('password').isLength({min: 6, max: 15}).withMessage('Password has to be between 6 and 15 characters.')
+const password = check ('password')
+    .isLength({min: 6, max: 15})
+    .withMessage('Password has to be between 6 and 15 characters.')
 
 //email
 
-const email = check('email').isEmail().withMessage('Please provide a valid email.')
+const email = check('email')
+    .isEmail()
+    .withMessage('Please provide a valid email.')
 
 //check if email exists
-const emailExists = check('email').custom(async(value) => {
-    const { rows } = await db.query(' SELECT * from users WHERE email = $1', [
+const emailExists = check('email').custom(async (value) => {
+    const { rows } = await db.query('SELECT * from users WHERE email = $1', [
         value,
     ])
     if (rows.length){
