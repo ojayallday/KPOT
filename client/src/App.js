e5a84c9b5b98dfeb332b6856c4892d94c3142c5b
deusnyakundi <nyakundideus1@gmail.com>
2023-11-28 17:09:57 +0300

    Creating routes for the pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,30 @@
+import { 
+  BrowserRouter,
+  Navigate,
+  Routes,
+  Route,
+  Outlet,
+
+ } from "react-router-dom";
+import Home from "./pages/home";
+import Dashboard from "./pages/dashboard";
+import Register from "./pages/register";
+import Login from "./pages/login";
+
+
+
 const App = () => {
 
-  return <div> <h1>Hello</h1></div>
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path= '/' element = {<Home />}/>
+      <Route path= '/dashboard' element = {<Dashboard />}/>
+      <Route path= '/register' element = {<Register />}/>
+      <Route path= '/login' element = {<Login />}/>
+    </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
