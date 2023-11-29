ce935388b4262efa5ac92d6ffbd2a20f33da02b0
deusnyakundi <nyakundideus1@gmail.com>
2023-11-29 04:31:21 +0300

    Creating form for registration

--- client/src/pages/register.js
+++ client/src/pages/register.js
@@ -11,17 +11,61 @@ const Register = () => {
     const [error, setError] = useState(false)
     const [success, setSucess] = useState(false)
 
-    const onChange = () => {
+    const onChange = (e) => {
+        setValues({ ...values, [e.target.name]: e.target.value })
 
     }
 
-    const onSubmit = () => {
-        
+    const onSubmit = (e) => {
+
+        e.preventDefault()
     }
 
     return (
         <Layout>
-             <h1>Register</h1>
+    
+             <form onSubmit={(e) => onSubmit(e)} className='container mt-3'>
+        <h1>Register</h1>
+
+        <div className='mb-3'>
+          <label htmlFor='email' className='form-label'>
+            Email address
+          </label>
+          <input
+            onChange={(e) => onChange(e)}
+            type='email'
+            className='form-control'
+            id='email'
+            name='email'
+            value={values.email}
+            placeholder='test@gmail.com'
+            required
+          />
+        </div>
+
+        <div className='mb-3'>
+          <label htmlFor='password' className='form-label'>
+            Password
+          </label>
+          <input
+            onChange={(e) => onChange(e)}
+            type='password'
+            value={values.password}
+            className='form-control'
+            id='password'
+            name='password'
+            placeholder='Please enter your password'
+            required
+          />
+        </div>
+
+        <div style={{ color: 'red', margin: '10px 0' }}>{error}</div>
+        <div style={{ color: 'green', margin: '10px 0' }}>{success}</div>
+
+        <button type='submit' className='btn btn-primary'>
+          Submit
+        </button>
+      </form>
         </Layout>
     )
   }
