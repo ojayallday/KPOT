2d7b9102fd384087b85d4afc6eb45cb25e1fb814
deusnyakundi <nyakundideus1@gmail.com>
2023-11-29 04:19:16 +0300

    Building functionality for register

--- client/src/pages/register.js
+++ client/src/pages/register.js
@@ -1,6 +1,23 @@
+import { useState } from "react";
 import Layout from "../components/layout";
 
 const Register = () => {
+    const [values, setValues] =useState ({
+        email: '',
+        password: ''
+
+    })
+
+    const [error, setError] = useState(false)
+    const [success, setSucess] = useState(false)
+
+    const onChange = () => {
+
+    }
+
+    const onSubmit = () => {
+        
+    }
 
     return (
         <Layout>
