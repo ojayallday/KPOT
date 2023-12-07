4c7f40aa0c8f0f6f09a828f5bda4ad4e66ce29bb
Julius Ojakaa <ojayallday@gmail.com>
2023-12-07 18:46:18 +0300

    Charts and cards added to dashboard with dummy data

--- client/src/components/ProjectsCard1.jsx
+++ client/src/components/ProjectsCard1.jsx
@@ -1,11 +1,26 @@
 // Card.js
 import React from 'react';
+import { Pie } from 'react-chartjs-2';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+ChartJS.register(ArcElement , Tooltip , Legend);
 
 const ProjectsCard1 = ({ title, content }) => {
+    
+    const data = {
+        labels: ['Category 1', 'Category 2', 'Category 3'],
+        datasets: [
+          {
+            data: [30, 40, 30],
+            backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
+            hoverBackgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
+          },
+        ],
+      };
+
   return (
     <div style={{ border: '1px solid #ddd', borderRadius: '8px', padding: '16px', margin: '16px', maxWidth: '300px' }}>
       <h3>Project Summary</h3>
-      <p>{content}</p>
+      <Pie data={data} />
     </div>
   );
 };
--- client/src/components/ProjectsCard2.jsx
+++ client/src/components/ProjectsCard2.jsx
@@ -1,11 +1,27 @@
 // Card.js
 import React from 'react';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Doughnut } from 'react-chartjs-2';
+ChartJS.register(ArcElement , Tooltip , Legend);
+
 
 const ProjectsCard2 = ({ title, content }) => {
+
+    const data = {
+        labels: ['Category 1', 'Category 2', 'Category 3'],
+        datasets: [
+          {
+            data: [30, 40, 30],
+            backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
+            hoverBackgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
+          },
+        ],
+      };
+
   return (
     <div style={{ border: '1px solid #ddd', borderRadius: '8px', padding: '16px', margin: '16px', maxWidth: '300px' }}>
-      <h3>{title}</h3>
-      <p>{content}</p>
+      <h3>{title}OAC PROJECTS</h3>
+      <Doughnut data={data} />
     </div>
   );
 };
