c0fcfc74a4366f22a1eb4037e2bf579294a09ec0
deusnyakundi <nyakundideus1@gmail.com>
2023-12-08 11:30:08 +0300

    Resolving remaing conflict in db/index.js

--- server/src/db/index.js
+++ server/src/db/index.js
@@ -4,11 +4,8 @@ const pool = new Pool({
     host: 'localhost',
     database: 'safpro',
     password: 'superadmin',
-<<<<<<< HEAD
     port: 5433,
-=======
-    port: 5432,
->>>>>>> 107b3bd9944e31e54780e9b226e511c81d57d5d1
+
 })
 module.exports = {
     query: (text, params) => pool.query (text, params),
