107b3bd9944e31e54780e9b226e511c81d57d5d1
Moses Mbugua <mmmuteti@gmail.com>
2023-12-08 07:59:49 +0300

    Committing changes before switching branches

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -3,19 +3,19 @@ axios.defaults.withCredentials = true
 
 export async function onRegistration(registrationData) {
   return await axios.post(
-    'http://localhost:8000/api/register',
+    'http://localhost:8001/api/register',
     registrationData
   )
 }
 
 export async function onLogin(loginData) {
-  return await axios.post('http://localhost:8000/api/login', loginData)
+  return await axios.post('http://localhost:8001/api/login', loginData)
 }
 
 export async function onLogout() {
-  return await axios.get('http://localhost:8000/api/logout')
+  return await axios.get('http://localhost:8001/api/logout')
 }
 
 export async function fetchProtectedInfo() {
-  return await axios.get('http://localhost:8000/api/protected')
+  return await axios.get('http://localhost:8001/api/protected')
 }
