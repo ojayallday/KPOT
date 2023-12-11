ea4e4e8556f2b6f7af4cc1b57b3b9915170465c8
deusnyakundi <nyakundideus1@gmail.com>
2023-12-11 06:41:31 +0300

    Updating db configs and .env to allow app to work on render and localhost

--- server/src/db/index.js
+++ server/src/db/index.js
@@ -1,11 +1,18 @@
 const {Pool} =  require('pg');
 const express =require('express');
+const { config } = require('dotenv');
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'safpro',
-    password: 'superadmin',
-    port: 5433,
+   // user: 'postgres',
+  //   host: 'localhost',
+  //  database: 'safpro',
+ //   password: 'superadmin',
+  //  port: 5433,
+
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
 })
 module.exports = {
     query: (text, params) => pool.query (text, params),
